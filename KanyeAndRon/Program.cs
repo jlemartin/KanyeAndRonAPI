92a900c59d5b76eadc854d4e2980518e736c7773
John Martin <jlemartin7@gmail.com>
2020-11-30 13:21:36 -0600

    Completed Exercise

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=5; i++)
+            {
+                Console.WriteLine($"Dialog #{i}");
+
+                Console.WriteLine($"Kanye: \"{Quoter.Kanye()}\"");
+
+                Console.WriteLine($"Ron: {Quoter.Ron()}");
+
+                Console.WriteLine();
+
+            }
+        }
+
+    }
+}
---
+++ KanyeAndRon/Quoter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeAndRon
+{
+    public class Quoter
+    {
+        public static string Kanye()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public static string Ron()
+        {
+            var client = new HttpClient();
+
+            var RonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(RonURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;
+
+        }
+
+    }
+}
